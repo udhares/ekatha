2c6a6acafd21d87db3b915ecc001d76b34d5771d
udhares <46632861+udhares@users.noreply.github.com>
2024-07-30 15:21:41 +0500

    Create abc.js

---
+++ Javascript/Adding two numbers with function/abc.js
@@ -0,0 +1,8 @@
+function areacalc(){
+	var furathama = document.getElementById("num1").value;
+	var dhevana = document.getElementById("num2").value;
+	
+	var natheeja = furathama * dhevana;
+	
+	document.getElementById("result").innerHTML = natheeja;
+}
